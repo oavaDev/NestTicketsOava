59db92ea74b520bb72bd4ccabd6e7a881b79451e
ovega <omarvega598@gmail.com>
2024-01-26 10:24:42 -0500

    Feat(Bcrypt): Add hashing to user passwords

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,8 @@
 import { Controller, Get, Post, Body, Delete } from '@nestjs/common';
 import { UserService } from './user.service';
 import { createUserDto } from './dto/createUser.dto';
-import {deleteUserDto} from "./dto/deleteUser.dto";
+import { deleteUserDto } from './dto/deleteUser.dto';
+import { loginUserDto } from './dto/loginUser.dto';
 
 @Controller('user')
 export class UserController {
@@ -10,9 +11,13 @@ export class UserController {
   findAll() {
     return this.userService.findAll();
   }
-  @Post()
-  create(@Body() createUserDto: createUserDto): Promise<createUserDto> {
-    return this.userService.create(createUserDto);
+  @Post('login')
+  login(@Body() user: loginUserDto) {
+    return this.userService.login(user);
+  }
+  @Post('register')
+  register(@Body() createUserDto: createUserDto): Promise<createUserDto> {
+    return this.userService.register(createUserDto);
   }
   @Delete()
   delete(@Body() user: deleteUserDto): string {
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,6 +3,8 @@ import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schemas/user.schema';
 import { Model } from 'mongoose';
 import { createUserDto } from './dto/createUser.dto';
+import { loginUserDto } from './dto/loginUser.dto';
+import { generateHashedPassword, verifyHashAndPassword } from '../auth/bcrypt/bcrypt/bcrypt';
 
 @Injectable()
 export class UserService {
@@ -10,8 +12,21 @@ export class UserService {
   async findAll(): Promise<User[]> {
     return this.userModel.find().exec();
   }
-  async create(createCat: createUserDto) {
-    const createdUser = new this.userModel(createCat);
+  async login(userToLogin: loginUserDto) {
+    const user = await this.userModel.findOne({ email: userToLogin.email });
+    const passwordsMatch = await verifyHashAndPassword(
+      userToLogin.password,
+      user.password,
+    );
+    if (passwordsMatch) {
+      return user;
+    } else {
+      return 'Invalid credentials';
+    }
+  }
+  async register(user: createUserDto) {
+    user.password = await generateHashedPassword(user.password);
+    const createdUser = new this.userModel(user);
     return createdUser.save();
   }
   async delete(email: string) {
